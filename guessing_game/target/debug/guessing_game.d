cfcf2fd3e644698b54df4f8a6a75a16afca48bce
Marco Rossel <andresmarcorossel@gmail.com>
2026-02-19 10:47:06 -0300

    rust | documentation

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\andre\projects\guessing_game\target\debug\guessing_game.exe: C:\Users\andre\projects\guessing_game\src\main.rs
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\andre\projects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\andre\projects\hello_cargo\src\main.rs
